feaa905bb5fcf3e8237559e3c01705d48165519a
gck <gck@ableton.com>
2015-02-24 23:53:29 +0100

    Move scanning sources into separate function

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,8 @@
 
 
 namespace fs = boost::filesystem;
+using namespace boost::posix_time;
+using namespace boost::gregorian;
 
 namespace {
 
@@ -79,6 +81,42 @@ setup_scheme_context(const fs::path& prefix_path, const fs::path& module_path)
   }
 
   return std::move(ctx);
+}
+
+
+eyestep::Node scan_sources(const std::vector<eyestep::Source>& sources,
+                           const std::vector<std::string>& incl_paths,
+                           const std::vector<std::string>& defs)
+{
+  eyestep::Node root("project");
+  root["start-time"] = to_iso_extended_string(microsec_clock::local_time());
+
+  if (!sources.empty()) {
+    for (const auto& src : sources) {
+      std::cout << "Scan " << src.mSrcfile << " ...";
+      std::cout.flush();
+
+      std::unique_ptr<eyestep::IScanner> scanner =
+          make_scanner_for_file(src.mSrcfile);
+
+      if (scanner) {
+        eyestep::Node nd =
+            scanner->scanFile(src.mSrcfile,
+                              eyestep::utils::joinList(incl_paths,
+                                                       src.mLocIncls),
+                              eyestep::utils::joinList(defs, src.mLocDefs));
+        std::cout << " ok" << std::endl;
+
+        root.addChildNode(nd);
+      }
+      else {
+        std::cout << " no scanner for filetype" << std::endl;
+      }
+    }
+  }
+
+  root["end-time"] = to_iso_extended_string(microsec_clock::local_time());
+  return root;
 }
 
 } // anon namespace
@@ -208,39 +246,9 @@ int main(int argc, char** argv)
 
     auto scheme_ctx = std::move(setup_scheme_context(prefix_path, module_path));
 
-    using namespace boost::posix_time;
-    using namespace boost::gregorian;
+    eyestep::Node root = scan_sources(sources, incl_paths, defs);
+    eyestep::serialize(std::cout, root);
 
-    if (!sources.empty()) {
-      eyestep::Node root("project");
-      root["start-time"] = to_iso_extended_string(microsec_clock::local_time());
-
-      for (const auto& src : sources) {
-        std::cout << "Scan " << src.mSrcfile << " ...";
-        std::cout.flush();
-
-        std::unique_ptr<eyestep::IScanner> scanner =
-            make_scanner_for_file(src.mSrcfile);
-
-        if (scanner) {
-          eyestep::Node nd =
-              scanner->scanFile(src.mSrcfile,
-                                eyestep::utils::joinList(incl_paths,
-                                                         src.mLocIncls),
-                                eyestep::utils::joinList(defs, src.mLocDefs));
-          std::cout << " ok" << std::endl;
-
-          root.addChildNode(nd);
-        }
-        else {
-          std::cout << " no scanner for filetype" << std::endl;
-        }
-      }
-
-      root["end-time"] = to_iso_extended_string(microsec_clock::local_time());
-
-      eyestep::serialize(std::cout, root);
-    }
   }
   catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
