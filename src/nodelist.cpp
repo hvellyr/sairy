51f7bf1b14b5642407160116e80c13fb42d3620f
hvellyr <gck@eyestep.org>
2015-03-20 22:26:26 +0100

    Refactored NodeList
    ... to simplify extending it.

--- src/nodelist.cpp
+++ src/nodelist.cpp
@@ -4,221 +4,272 @@
 #include "nodelist.hpp"
 #include "estd/memory.hpp"
 
+#include <memory>
 #include <algorithm>
 #include <cassert>
 
 
 namespace eyestep {
 
+using detail::SiblingsNodeListStrategy;
+using detail::AncestorsNodeListStrategy;
+using detail::DescendantsNodeListStrategy;
+
+
 //------------------------------------------------------------------------------
 
-NodeList::NodeList() : NodeList(nullptr, nullptr, kEmpty, 0, 0, 0, {})
+NodeList::NodeList()
 {
 }
 
 
 NodeList::NodeList(const Node* nd, Kind kind)
-    : NodeList(nd, nullptr, kind, 0, -1, -1, {})
 {
-  assert(mNode);
+  assert(nd);
 
-  switch (mKind) {
-  case kEmpty:
-    break;
-  case kChildren:
-    mEnd = int(mNode->property<Nodes>(CommonProps::kChildren).size());
-    mKind = mStart >= mEnd ? kEmpty : mKind;
-    break;
-  case kSiblings:
-    mParent = mNode->parent();
-    mEnd = int(mParent->property<Nodes>(CommonProps::kChildren).size());
-    mKind = mStart >= mEnd ? kEmpty : mKind;
-    break;
-  case kPreced:
-    mParent = mNode->parent();
-    {
-      const Nodes& nl = mParent->property<Nodes>(CommonProps::kChildren);
-      auto i_find = std::find_if(nl.begin(), nl.end(),
-                                 [&](const Node* n) { return nd == n; });
-      mEnd = std::distance(nl.begin(), i_find);
-      mKind = mStart >= mEnd ? kEmpty : mKind;
+  if (kind == kChildren) {
+    int end = int(nd->property<Nodes>(CommonProps::kChildren).size());
+    if (end > 0) {
+      mStrategy = estd::make_unique<SiblingsNodeListStrategy>(nd, 0, end);
     }
-    break;
-  case kFollow:
-    mParent = mNode->parent();
-    {
-      const Nodes& nl = mParent->property<Nodes>(CommonProps::kChildren);
-      auto i_find = std::find_if(nl.begin(), nl.end(),
-                                 [&](const Node* n) { return nd == n; });
-      mStart = std::distance(nl.begin(), i_find) + 1;
-      mEnd = int(nl.size());
-      mKind = mStart >= mEnd ? kEmpty : mKind;
+  }
+  else if (kind == kSiblings) {
+    const auto* parent = nd->parent();
+    int end = int(parent->property<Nodes>(CommonProps::kChildren).size());
+    if (end > 0) {
+      mStrategy = estd::make_unique<SiblingsNodeListStrategy>(parent, 0, end);
     }
-    break;
-  case kAncestors:
-    mNode = mNode->parent();
-    mCount = -1;
-    break;
-  case kDescendants:
-    mStart = 0;
-    mParent = mNode;
-    mEnd = int(mNode->property<Nodes>(CommonProps::kChildren).size());
-    mKind = mStart >= mEnd ? kEmpty : mKind;
-    break;
+  }
+  else if (kind == kPreced) {
+    const auto* parent = nd->parent();
+    const Nodes& nl = parent->property<Nodes>(CommonProps::kChildren);
+    auto i_find = std::find_if(nl.begin(), nl.end(),
+                               [&](const Node* n) { return nd == n; });
+    int end = std::distance(nl.begin(), i_find);
+    if (end > 0) {
+      mStrategy = estd::make_unique<SiblingsNodeListStrategy>(parent, 0, end);
+    }
+  }
+  else if (kind == kFollow) {
+    const auto* parent = nd->parent();
+    const Nodes& nl = parent->property<Nodes>(CommonProps::kChildren);
+    auto i_find = std::find_if(nl.begin(), nl.end(),
+                               [&](const Node* n) { return nd == n; });
+    int start = std::distance(nl.begin(), i_find) + 1;
+    int end = int(nl.size());
+
+    if (start < end) {
+      mStrategy =
+        estd::make_unique<SiblingsNodeListStrategy>(parent, start, end);
+    }
+  }
+  else if (kind == kAncestors) {
+    mStrategy = estd::make_unique<AncestorsNodeListStrategy>(nd->parent(), -1);
+  }
+  else if (kind == kDescendants) {
+    int end = int(nd->property<Nodes>(CommonProps::kChildren).size());
+    if (end > 0) {
+      mStrategy =
+        estd::make_unique<DescendantsNodeListStrategy>(nd, 0, end, -1,
+                                                       std::vector<int>{});
+    }
   }
 }
 
 
-NodeList::NodeList(const Node* nd, const Node* parent, Kind kind, int start,
-                   int end, int count, std::vector<int> stack)
-    : mNode(nd), mParent(parent), mKind(kind), mStart(start), mEnd(end),
-      mCount(count), mStack(std::move(stack))
+NodeList::NodeList(std::unique_ptr<detail::INodeListStrategy> strategy)
+  : mStrategy(std::move(strategy))
 {
-  assert(mNode || mKind == kEmpty);
 }
 
 
 bool NodeList::empty() const
 {
-  switch (mKind) {
-  case kEmpty:
-    return true;
-  case kChildren:
-  case kSiblings:
-  case kPreced:
-  case kFollow:
-    return mEnd - mStart == 0;
-  case kAncestors:
-    return mCount == 0 || mNode == nullptr;
-  case kDescendants:
-    if (mCount < 0) {
-      return mKind == kEmpty;
-    }
-    return mCount == 0;
-  }
-  return true;
+  return mStrategy ? mStrategy->empty() : true;
 }
 
 
 int NodeList::length() const
 {
-  switch (mKind) {
-  case kEmpty:
-    return 0;
-  case kChildren:
-  case kSiblings:
-  case kPreced:
-  case kFollow:
-    return mEnd - mStart;
-  case kAncestors:
-    if (mCount < 0) {
-      const Node* p = mNode;
-      int nc = 0;
-      while (p) {
-        nc++;
-        p = p->parent();
-      }
-      const_cast<NodeList*>(this)->mCount = nc;
-    }
-    return mCount;
-  case kDescendants:
-    if (mCount < 0) {
-      int nc = 1;
-      NodeList nl = rest();
-      while (nl.mKind != kEmpty) {
-        nl = nl.rest();
-        nc++;
-      }
-
-      const_cast<NodeList*>(this)->mCount = nc;
-    }
-    return mCount;
-  }
-  return 0;
+  return mStrategy ? mStrategy->length() : 0;
 }
 
 
 const Node* NodeList::head() const
 {
-  switch (mKind) {
-  case kEmpty:
-    return nullptr;
-  case kChildren:
-    assert(mStart < mEnd);
-    return mNode->property<Nodes>(CommonProps::kChildren)[mStart];
-  case kSiblings:
-  case kPreced:
-  case kFollow:
-    assert(mStart < mEnd);
-    return mParent->property<Nodes>(CommonProps::kChildren)[mStart];
-  case kAncestors:
-    return mNode;
-  case kDescendants:
-    assert(mStart < mEnd);
-    return mNode->property<Nodes>(CommonProps::kChildren)[mStart];
-  }
-
-  return nullptr;
+  return mStrategy ? mStrategy->head() : nullptr;
 }
 
 
 NodeList NodeList::rest() const
 {
-  switch (mKind) {
-  case kEmpty:
-    return *this;
-  case kChildren:
-  case kSiblings:
-  case kPreced:
-  case kFollow:
-    if (mStart + 1 < mEnd) {
-      return NodeList(mNode, mParent, mKind, mStart + 1, mEnd, mCount, mStack);
-    }
-    break;
-  case kAncestors:
-    if (Node* p = mNode->parent()) {
-      return NodeList(p, nullptr, kAncestors, 0, 0, mCount - 1, mStack);
-    }
-    break;
-  case kDescendants:
-    if (mStart < mEnd) {
-      Node* newNode = mNode->property<Nodes>(CommonProps::kChildren)[mStart];
+  return mStrategy ? mStrategy->rest() : NodeList();
+}
 
-      const Nodes& children = newNode->property<Nodes>(CommonProps::kChildren);
-      if (children.size() > 0) {
-        std::vector<int> stack(mStack);
-        stack.emplace_back(mStart + 1);
-        return NodeList(newNode, nullptr, kDescendants, 0,
-                        int(newNode->property<Nodes>(CommonProps::kChildren)
-                                .size()),
-                        mCount - 1, std::move(stack));
+
+//------------------------------------------------------------------------------
+
+SiblingsNodeListStrategy::SiblingsNodeListStrategy(const Node* node, int start,
+                                                   int end)
+  : mNode(node), mStart(start), mEnd(end)
+{
+}
+
+bool SiblingsNodeListStrategy::empty() const
+{
+  return mEnd - mStart == 0;
+}
+
+int SiblingsNodeListStrategy::length() const
+{
+  return mEnd - mStart;
+}
+
+const Node* SiblingsNodeListStrategy::head() const
+{
+  assert(mStart < mEnd);
+  return mNode->property<Nodes>(CommonProps::kChildren)[mStart];
+}
+
+NodeList SiblingsNodeListStrategy::rest() const
+{
+  if (mStart + 1 < mEnd) {
+    return NodeList(std::move(
+      estd::make_unique<SiblingsNodeListStrategy>(mNode, mStart + 1, mEnd)));
+  }
+  return NodeList();
+}
+
+
+//------------------------------------------------------------------------------
+
+AncestorsNodeListStrategy::AncestorsNodeListStrategy(const Node* parent,
+                                                     int count)
+  : mNode(parent), mCount(count)
+{
+}
+
+bool AncestorsNodeListStrategy::empty() const
+{
+  return mCount == 0 || mNode == nullptr;
+}
+
+int AncestorsNodeListStrategy::length() const
+{
+  if (mCount < 0) {
+    const Node* p = mNode;
+    int nc = 0;
+    while (p) {
+      nc++;
+      p = p->parent();
+    }
+    const_cast<AncestorsNodeListStrategy*>(this)->mCount = nc;
+  }
+  return mCount;
+}
+
+const Node* AncestorsNodeListStrategy::head() const
+{
+  return mNode;
+}
+
+NodeList AncestorsNodeListStrategy::rest() const
+{
+  if (Node* p = mNode->parent()) {
+    return NodeList(
+      estd::make_unique<AncestorsNodeListStrategy>(p, mCount - 1));
+  }
+  return NodeList();
+}
+
+
+//------------------------------------------------------------------------------
+
+DescendantsNodeListStrategy::DescendantsNodeListStrategy(const Node* node,
+                                                         int start, int end,
+                                                         int count,
+                                                         std::vector<int> stack)
+  : mNode(node), mStart(start), mEnd(end), mCount(count),
+    mStack(std::move(stack))
+{
+}
+
+bool DescendantsNodeListStrategy::empty() const
+{
+  if (mCount < 0) {
+    return length() == 0;
+  }
+  return mCount == 0;
+}
+
+int DescendantsNodeListStrategy::length() const
+{
+  if (mCount < 0) {
+    int nc = 1;
+    NodeList nl = rest();
+    while (nl.mStrategy.get() != nullptr) {
+      nl = nl.rest();
+      nc++;
+    }
+
+    const_cast<DescendantsNodeListStrategy*>(this)->mCount = nc;
+  }
+  return mCount;
+}
+
+const Node* DescendantsNodeListStrategy::head() const
+{
+  assert(mStart < mEnd);
+  return mNode->property<Nodes>(CommonProps::kChildren)[mStart];
+}
+
+NodeList DescendantsNodeListStrategy::rest() const
+{
+  if (mStart < mEnd) {
+    Node* newNode = mNode->property<Nodes>(CommonProps::kChildren)[mStart];
+
+    const Nodes& children = newNode->property<Nodes>(CommonProps::kChildren);
+    if (children.size() > 0) {
+      std::vector<int> stack(mStack);
+      stack.emplace_back(mStart + 1);
+
+      return NodeList(estd::make_unique<
+        DescendantsNodeListStrategy>(newNode, 0,
+                                     int(newNode->property<Nodes>(
+                                                    CommonProps::kChildren)
+                                           .size()),
+                                     mCount - 1, std::move(stack)));
+    }
+  }
+
+  if (mStart + 1 < mEnd) {
+    return NodeList(
+      estd::make_unique<DescendantsNodeListStrategy>(mNode, mStart + 1, mEnd,
+                                                     mCount - 1, mStack));
+  }
+
+  if (mStack.size() > 0) {
+    Node* p = mNode->parent();
+    auto i_last = std::prev(mStack.end());
+
+    while (p && i_last >= mStack.begin()) {
+      auto end = int(p->property<Nodes>(CommonProps::kChildren).size());
+      auto start = *i_last;
+      if (start < end) {
+        std::vector<int> stack(mStack.begin(), i_last);
+        return NodeList(
+          estd::make_unique<DescendantsNodeListStrategy>(p, start, end,
+                                                         mCount - 1,
+                                                         std::move(stack)));
       }
-    }
-    if (mStart + 1 < mEnd) {
-      return NodeList(mNode, nullptr, kDescendants, mStart + 1, mEnd,
-                      mCount - 1, mStack);
-    }
-    if (mStack.size() > 0) {
-      Node* p = mNode->parent();
-      auto i_last = std::prev(mStack.end());
 
-      while (p && i_last >= mStack.begin()) {
-        auto end = int(p->property<Nodes>(CommonProps::kChildren).size());
-        auto start = *i_last;
-        if (start < end) {
-          std::vector<int> stack(mStack.begin(), i_last);
-          return NodeList(p, nullptr, kDescendants, start, end, mCount - 1,
-                          std::move(stack));
-        }
-
-        p = p->parent();
-        --i_last;
-      }
+      p = p->parent();
+      --i_last;
     }
-    break;
   }
 
   return NodeList();
 }
+
 
 } // ns eyestep
--- src/nodelist.hpp
+++ src/nodelist.hpp
@@ -5,10 +5,72 @@
 
 #include "nodes.hpp"
 
+#include <memory>
 #include <vector>
 
 
 namespace eyestep {
+
+class NodeList;
+
+
+namespace detail {
+  class INodeListStrategy {
+  public:
+    virtual ~INodeListStrategy() { }
+
+    virtual bool empty() const = 0;
+    virtual int length() const = 0;
+    virtual const Node* head() const = 0;
+    virtual NodeList rest() const = 0;
+  };
+
+  class SiblingsNodeListStrategy : public INodeListStrategy  {
+  public:
+    SiblingsNodeListStrategy(const Node* node, int start, int end);
+
+    bool empty() const;
+    int length() const;
+    const Node* head() const;
+    NodeList rest() const;
+
+    const Node* mNode;
+    int mStart;
+    int mEnd;
+  };
+
+  class AncestorsNodeListStrategy : public INodeListStrategy  {
+  public:
+    AncestorsNodeListStrategy(const Node* parent, int count);
+
+    bool empty() const;
+    int length() const;
+    const Node* head() const;
+    NodeList rest() const;
+
+    const Node* mNode;
+    int mCount;
+  };
+
+  class DescendantsNodeListStrategy : public INodeListStrategy  {
+  public:
+    DescendantsNodeListStrategy(const Node* node, int start, int end, int count,
+                                std::vector<int> stack);
+
+    bool empty() const;
+    int length() const;
+    const Node* head() const;
+    NodeList rest() const;
+
+    const Node* mNode;
+    int mStart;
+    int mEnd;
+    int mCount;
+    std::vector<int> mStack;
+  };
+
+} // detail ns
+
 
 class NodeList {
 public:
@@ -25,22 +87,18 @@ public:
   NodeList();
   NodeList(const Node* nd, Kind kind);
 
+  // internal
+  NodeList(std::unique_ptr<detail::INodeListStrategy> strategy);
+
   bool empty() const;
   int length() const;
   const Node* head() const;
   NodeList rest() const;
 
 private:
-  NodeList(const Node* nd, const Node* parent, Kind kind, int start, int end, int count,
-           std::vector<int> stack);
+  friend class detail::DescendantsNodeListStrategy;
 
-  const Node* mNode;
-  const Node* mParent;
-  Kind mKind;
-  int mStart;
-  int mEnd;
-  int mCount;
-  std::vector<int> mStack;
+  std::unique_ptr<detail::INodeListStrategy> mStrategy;
 };
 
 } // ns eyestep
