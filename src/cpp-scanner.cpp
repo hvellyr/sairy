d1eb5ccc30d542beb91dfa4f32fcbff3117c3cb6
hvellyr <gck@eyestep.org>
2015-03-21 01:16:05 +0100

    Change construction of scanner nodes

--- src/cpp-scanner.cpp
+++ src/cpp-scanner.cpp
@@ -78,8 +78,8 @@ namespace {
       Node* param = grove->makeNode(elementClassDefinition());
       param->setProperty("gi", "parameter");
       param->setProperty("name", std::get<0>(args_nm[i]));
-      param->setProperty("type", make_type_node(grove, "type",
-                                                std::get<1>(args_nm[i])));
+      param->addChildNode(make_type_node(grove, "type",
+                                         std::get<1>(args_nm[i])));
       parameters->addChildNode(param);
     }
 
