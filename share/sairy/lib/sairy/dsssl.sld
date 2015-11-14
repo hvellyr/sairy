5db18cabc76c0149760fc0e9f85c04d9d6f4ec66
hvellyr <gck@eyestep.org>
2015-11-14 20:11:49 +0100

    Implement inline-space and display-space
    ... by replacing the now unused %make-dimen .

--- share/sairy/lib/sairy/dsssl.sld
+++ share/sairy/lib/sairy/dsssl.sld
@@ -1,11 +1,11 @@
 (define-library (sairy dsssl)
-  (import (chibi) (srfi 9) (srfi 28) (srfi 39) (srfi 69)
+  (import (chibi) (srfi 9) (srfi 28) (srfi 39) (srfi 69) (srfi 89)
           (sairy string-lib))
   (export current-mode register-default-rule register-root-rule register-element-rule
           default element root mode text
           process-node-list process-node-list-trim
           process-children process-children-trim current-node
-          make literal
+          make literal inline-space display-space
           select-elements match-element?
           node-list
           sosofo-append
--- share/sairy/lib/sairy/fo.scm
+++ share/sairy/lib/sairy/fo.scm
@@ -19,3 +19,17 @@
     ((sosofo-append) (empty-sosofo))
     ((sosofo-append args ...) (%sosofo-append (list args ...)))
     ))
+
+(define* (inline-space q (min: min #f) (max: max #f))
+  (%make-length-spec 'inline q (if (not min) q min)
+                     (if (not max) q max)
+                     #f
+                     1))
+
+(define* (display-space q
+                        (min: min #f) (max: max #f)
+                        (conditional?: cond #f)
+                        (priority: prio 1))
+  (%make-length-spec 'display q (if (not min) q min)
+                     (if (not max) q max)
+                     cond prio))
