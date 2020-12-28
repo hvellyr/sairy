98ae79f13cc01e6fac21e2c4bc78239dde898c63
hvellyr <gck@eyestep.org>
2020-12-28 15:15:00 +0100

    Fix compiler warnings in tests

--- src/test/tst_main.cpp
+++ src/test/tst_main.cpp
@@ -1,6 +1,10 @@
 // Copyright (C) 2015 Gregor Klinke
 // All rights reserved.
 
+#include "../warnings.hpp"
+
 #define CATCH_CONFIG_MAIN
 
+TEXTBOOK_SUPPRESS_WARNINGS
 #include "catch/catch.hpp"
+TEXTBOOK_RESTORE_WARNINGS
