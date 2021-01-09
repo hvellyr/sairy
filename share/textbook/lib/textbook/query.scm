30fda92143aa832b8acf5d685d607b28fde6f352
hvellyr <gck@eyestep.org>
2021-01-09 17:25:01 +0100

    Fix select-elements and match-element?
    ... for multi-step patterns.  Additionally support matching multiple GIs at
    once.

--- share/textbook/lib/textbook/query.scm
+++ share/textbook/lib/textbook/query.scm
@@ -1,26 +1,49 @@
 ;; Copyright (c) 2017 by Gregor Klinke
 ;; All rights reserved.
 
-(define (match-element? pattern snl)
-  (let loop ((nlp snl)
-             (p pattern))
-    (cond
-     ((null? p) #t)
-     ((equal? (car p) (gi nlp)) (loop (parent nlp) (cdr p)) )
+(define (vector-any? pred? v)
+  (let ((vlen (vector-length v)))
+    (let loop ((i 0))
+      (if (>= i vlen)
+          #f
+          (if (pred? (vector-ref v i))
+              #t
+              (loop (+ i 1)))))))
 
-     (else #f))
-    ))
+
+(define (match-element-impl? pattern snl)
+  (let loop ((nd snl)
+             (p pattern))
+    (if (null? p)
+        #t
+        (let ((step (car p)))
+          (cond
+           ((equal? step (gi nd))
+            (loop (parent nd) (cdr p)) )
+
+           ((and (vector? step) (> (vector-length step) 1))
+            (case (vector-ref step 0)
+              ((gi:) (if (equal? (vector-ref step 1) (gi nd))
+                         (loop (parent nd) (cdr p))
+                         #f))
+              ((gis:) (let ((patterns (vector-ref step 1)))
+                        (if (and (list? patterns) (member (gi nd) patterns))
+                            (loop (parent nd) (cdr p))
+                            #f)))
+              (else #f)))
+           (else #f)))
+        )))
+
+(define (match-element? pattern snl)
+  (match-element-impl? (if (list? pattern)
+                           (reverse pattern)
+                           (list pattern)) snl))
 
 (define (select-elements nl pattern)
-  (let ((pattern-lst (if (list? pattern)
-                         pattern
-                         (list pattern))))
-    (let loop ((n nl)
-               (result (node-list)))
-      (if (node-list-empty? n)
-          result
-          (loop (node-list-rest n) (let ((snl (node-list-first n)))
-                                     (if (match-element? pattern-lst snl)
-                                         (node-list result snl)
-                                         result))) )
-      )))
+  (let* ((pattern-lst (if (list? pattern)
+                          (reverse pattern)
+                          (list pattern)))
+        (result (node-list-filter (lambda (snl)
+                                    (match-element-impl? pattern-lst snl))
+                                  nl)))
+    result))
